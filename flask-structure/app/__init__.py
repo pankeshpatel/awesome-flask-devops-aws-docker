f95b1b0724bde54aeff2ab9f7d5eac85b2db3382
Pankesh Patel <pankeshpatel@MacBook-Pro.local>
2020-12-11 11:46:57 +0530

    added flask structure

---
+++ flask-structure/app/__init__.py
@@ -0,0 +1,12 @@
+# Think of the __init__.py file as a contructor 
+# that pulls all of the parts of our application together 
+# into a package and then tells Python to treat it as a package!
+
+# We are importing Flask and setting up our app variable
+from flask import Flask 
+app = Flask(__name__)
+
+
+# Using this method, we can import multiple
+# python files into our Flask Application
+from app import views
---
+++ flask-structure/app/views.py
@@ -0,0 +1,13 @@
+
+
+# We're actually importing the app variable 
+# we created in the __init__.py.
+from app import app
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+@app.route("/about")
+def about():
+    return "All about Flask"
